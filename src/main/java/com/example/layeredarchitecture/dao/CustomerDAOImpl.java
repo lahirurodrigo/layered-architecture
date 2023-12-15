3d2eb5c01b2b45a4eb7c3f4de6b218620eb18909
lahirurodrigo <kmlahirurodrigo@gmail.com>
2023-12-15 11:44:15 +0530

    Save Customer Refactor

--- src/main/java/com/example/layeredarchitecture/dao/CustomerDAOImpl.java
+++ src/main/java/com/example/layeredarchitecture/dao/CustomerDAOImpl.java
@@ -3,10 +3,7 @@ package com.example.layeredarchitecture.dao;
 import com.example.layeredarchitecture.db.DBConnection;
 import com.example.layeredarchitecture.model.CustomerDTO;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class CustomerDAOImpl {
@@ -24,5 +21,16 @@ public class CustomerDAOImpl {
         }
 
         return getAllCustomer;
+    }
+
+    public boolean saveCustomer(CustomerDTO dto) throws SQLException, ClassNotFoundException {
+
+        Connection connection = DBConnection.getDbConnection().getConnection();
+        PreparedStatement pstm = connection.prepareStatement("INSERT INTO Customer (id,name, address) VALUES (?,?,?)");
+        pstm.setString(1, dto.getId());
+        pstm.setString(2, dto.getName());
+        pstm.setString(3, dto.getAddress());
+
+        return pstm.executeUpdate()>0;
     }
 }
